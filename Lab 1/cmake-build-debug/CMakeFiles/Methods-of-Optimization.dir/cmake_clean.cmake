ea95d718d77d7449524ea1065c6c4d072834e307
M4skEl <sivereliseu7@gmail.com>
2020-09-23 20:13:34 +0300

    Do first lab

---
+++ Lab 1/cmake-build-debug/CMakeFiles/Methods-of-Optimization.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Methods-of-Optimization.dir/main.cpp.o"
+  "Methods-of-Optimization"
+  "Methods-of-Optimization.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Methods-of-Optimization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
